b43dbb545b9e518b1bc1a6b9db6a656a5eb1312f
Muhammad Waseem <muhammadwaseem@Muhammads-MacBook-Pro.local>
2022-09-10 09:20:19 +0530

    modified:   count_occurence.py

--- count_occurence.py
+++ count_occurence.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 def main():
@@ -7,10 +6,10 @@ def main():
 
 
 def count(s):
-    if matches := re.findall(r"\b[u][m]\b", s, re.IGNORECASE):
+    if matches := re.findall(r"\bum\b", s, re.IGNORECASE):
         return len(matches)
     else:
-        pass
+        return 0
 
 if __name__ == "__main__":
     main()
